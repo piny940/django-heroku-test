baa6cec463109b6d0f785abbfebeb6ac703e7e8c
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-03 11:47:22 +0900

    Reflect css to test page

--- app/views.py
+++ app/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_view(request):
-    html = '''
-    <h1>Heroku Test!</h1>
-    <p>Hello World!</p>
-    '''
-    return HttpResponse(html)
+    template = 'index.html'
+    return render(request, template)
