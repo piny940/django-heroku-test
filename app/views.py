4ad91a60aa911ae929e7b9d02b7b83856c6359a1
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-03 11:20:48 +0900

    Add test page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_view(request):
+    html = '''
+    <h1>Heroku Test!</h1>
+    <p>Hello World!</p>
+    '''
+    return HttpResponse(html)
