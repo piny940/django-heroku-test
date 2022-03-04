4befcf78e42bdf8723088d1684bc9c023bec89d4
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-04 17:23:26 +0900

    Remove security code

--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h7(#1c%%pa)@q0um^5hf_1!1&gcvj71nzvlnv!%1^9*-)p&se*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
